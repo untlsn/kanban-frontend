061e879585a0bce2217adfad89876a77e2f7fc14
Filip Skoczeń <philipskoczen@gmail.com>
2022-06-22 23:04:12 +0200

    Add index

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,31 @@ import '~/assets/style/reset.css';
 import { Link, Title } from 'react-head';
 import type { Route } from '~/routes';
 import favicon from '~/assets/images/favicon.svg';
+import Header from '~/components/organisms/Header';
 
 export default function App({ routes }: { routes: Route[] }) {
+  useEffect(() => {
+    const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    const { classList } = document.body;
+
+    if (darkMode) {
+      classList.add('dark');
+    } else {
+      classList.remove('dark');
+    }
+  }, []);
+
   return (
-    <>
+    <div className="bg-white text-text dark:(bg-[#21212D] text-dark-text) min-h-screen flex flex-col">
       <Link rel="icon" type="image/svg+xml" href={favicon} />
       <Title>App</Title>
+      <Header />
       <Routes>
         {routes.map(({ path, Comp }) => (
           <Route key={path} path={path} element={<Comp />} />
         ))}
       </Routes>
-    </>
+    </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,28 @@
+import logo from '~/assets/images/logo.svg';
+
 export default function App() {
   return (
-    <div />
+    <main className="flex-1 flex flex-col items-center justify-center h-full">
+      <div className="flex items-center">
+        <h1 className="text-4xl max-w-90">
+          Prosta, ale uzyteczna aplikacja typu kanban
+        </h1>
+        <img src={logo} alt="logo" className="h-60" />
+      </div>
+      <div className="text-center">
+        <Link to="/login" className="mr-2 hocus:underline">
+          Zaloguj się
+        </Link>
+        <Link to="/register" className="ml-2 hocus:underline">
+          Zarejestruj się
+        </Link>
+        <p>
+          <span>Nie chcesz kota w worku? </span>
+          <Link to="/app" className="hocus:underline">
+            Wypróbuj!
+          </Link>
+        </p>
+      </div>
+    </main>
   );
 }
