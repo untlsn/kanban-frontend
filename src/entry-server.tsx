0cec61fb367d95a95d43e4d605488b9026a5b2f2
Filip Skoczeń <90275016+untlsn@users.noreply.github.com>
2022-06-22 19:24:31 +0200

    Initial commit

---
+++ src/entry-server.tsx
@@ -0,0 +1,21 @@
+import ReactDomServer from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
+import { HeadProvider as _HeadProvider } from 'react-head';
+import { FC } from 'react';
+import App from './App';
+import { getRoutes } from '~/routes';
+
+const { routes, names } = getRoutes();
+
+const HeadProvider = _HeadProvider as FC<{ headTags: any[], children: any }>;
+
+export function render(url: string, headTags = []) {
+  return ReactDomServer.renderToString(
+    <HeadProvider headTags={headTags}>
+      <StaticRouter location={url}>
+        <App routes={routes} />
+      </StaticRouter>
+    </HeadProvider>,
+  );
+}
+export { names };
