9d7624e7f83ed2ca8888b8f6fd626db4e66e9327
Filip Skoczeń <philipskoczen@gmail.com>
2022-06-24 23:24:29 +0200

    Add add task button

--- src/components/organisms/KanbanMain/index.tsx
+++ src/components/organisms/KanbanMain/index.tsx
@@ -1,5 +1,14 @@
 import { observer } from 'mobx-react-lite';
 import kanban from '~/store/kanban';
+import Button from '~/components/atom/Button';
+
+function Title(props: { children: any }) {
+  return (
+    <h1 className="p-(y-4 x-8) w-full bg-primary dark:bg-dark-primary border-(t-2 l-2 gray-800) uppercase text-2xl">
+      {props.children}
+    </h1>
+  );
+}
 
 const KanbanMain = observer(() => {
   const { selectedBoard } = kanban;
@@ -7,21 +16,27 @@ const KanbanMain = observer(() => {
   if (!selectedBoard) {
     return (
       <main className="flex-1">
-        <h1>
-          Zaznacz tablicze
-        </h1>
+        <Title>
+          Stwórz tablicze
+        </Title>
       </main>
     );
   }
 
   return (
     <main className="flex-1">
-      <h1>
-        <O>{() => (
-          selectedBoard.name
-        )}
-        </O>
-      </h1>
+      <Title>
+        <div className="flex justify-between">
+          <O>
+            {() => (
+              selectedBoard.name
+            )}
+          </O>
+          <Button className="text-lg font-500 bg-violet-500 p-(x-4 y-1) rounded-full transition-opacity hover:opacity-70">
+            +Dodaj zadanie
+          </Button>
+        </div>
+      </Title>
     </main>
   );
 });
